18f63c49d6669a1a6b2675d4d52afd480d2cfae3
michael2904 <michael.abdallah.minciotti@gmail.com>
2017-02-06 22:37:18 -0500

    First version of the schema
    Marco, Eduardo, Michael worked and finished a first version of the
    schema.
    
    Need to fix the seeder.js to take care of constraints.
    
    When it is done, then we need to test the schema.

--- database/schema.js
+++ database/schema.js
@@ -1,81 +1,119 @@
 var schema = {};
 
-// Add SQL for Creating tables here! Below is an example for the users table
-
-// type = {Admin, Organizer, Team Lead, Participant}
 schema.users = {
     "name": "users",
     "fields" : {
-        "id" : "int(11) NOT NULL",
-        "firstname": "varchar(40) NOT NULL",
-        "lastname" : "varchar(40) NOT NULL",
-        "email" : "varchar(50) NOT NULL",
+        "id" : "int(11) NOT NULL AUTO_INCREMENT",
+        "firstName": "varchar(200) NOT NULL",
+        "lastName" : "varchar(200) NOT NULL",
+        "email" : "varchar(200) NOT NULL",
         "password" : "varchar(50) NOT NULL",
-        "type" : "varchar(50) NOT NULL",
+        "phoneNumber" : "varchar(50)",
+        "profilePicture" : "BLOB",
         "createdAt" : "datetime NOT NULL",
         "updatedAt" : "datetime NOT NULL"
     },
-    "post" : [
-        "ALTER TABLE `users`ADD PRIMARY KEY (`id`);",
-        "ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT"
-    ]
-}
-
-schema.scunt = {
-    "name": "scunt",
-    "fields": {
-        "id": "int(11) NOT NULL",
-        "teams": ["teams.id"],
-        "organizer": ["users.id"]
-        "start_time": "datetime NOT NULL",
-        "end_time": "datetime NOT NULL",
-        "createdAt" : "datetime NOT NULL",
-        "updatedAt" : "datetime NOT NULL"
+    "constraints": {
+        "PRIMARY KEY (id)"
     },
     "post" : [
-        "ALTER TABLE `scunt`ADD PRIMARY KEY (`id`);",
-        "ALTER TABLE `scunt` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT"
-    ]
-}
-
-schema.items = {
-    "name": "items",
-    "fields": {
-        "id": "int(11) NOT NULL",
-        "name": "varchar(50) NOT NULL",
-        "description": "varchar(500)",
-        "points": "int(5) NOT NULL",
-        // TODO foreign key
-        "scunt_id": "scunt.id",
-        "createdAt" : "datetime NOT NULL",
-        "updatedAt" : "datetime NOT NULL"
-    },
-    "post" : [
-        "ALTER TABLE `items`ADD PRIMARY KEY (`id`);",
-        "ALTER TABLE `items` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT"
     ]
 }
 
 schema.teams = {
     "name": "teams",
     "fields": {
-        "id": "int(11) NOT NULL",
-        // TODO foreign key to user id
-        "leader":  "users.id",
-        // TODO array of foreign keys
-        "members": ["users.id"],
-        "name": "varchar(20) NOT NULL",
-        "points": "int(5) NOT NULL",
-        // TODO foreign keys
-        "scunt": "users.scunt",
+        "id": "int(11) NOT NULL AUTO_INCREMENT",
+        "name": "varchar(50) NOT NULL",
+        "points": "int(10) NOT NULL",
+        "scuntId": "int(11) NOT NULL",
         "createdAt" : "datetime NOT NULL",
         "updatedAt" : "datetime NOT NULL"
     },
+    "constraints": {
+        "PRIMARY KEY (id)",
+        "FOREIGN KEY (scunt) REFERENCES scunt(id)"
+    },
     "post" : [
-        "ALTER TABLE `teams`ADD PRIMARY KEY (`id`);",
-        "ALTER TABLE `teams` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT"
     ]
 }
-// End of example
+
+schema.tasks = {
+    "name": "tasks",
+    "fields": {
+        "id": "int(11) NOT NULL AUTO_INCREMENT",
+        "name": "varchar(50) NOT NULL",
+        "description": "varchar(500)",
+        "points": "int(10) NOT NULL",
+        "scuntId": "int(11) NOT NULL",
+        "createdAt" : "datetime NOT NULL",
+        "updatedAt" : "datetime NOT NULL"
+    },
+    "constraints": {
+        "PRIMARY KEY (id)",
+        "FOREIGN KEY (scuntId) REFERENCES scunt(id)"
+    },
+    "post" : [
+    ]
+}
+
+schema.scunt = {
+    "name": "scunt",
+    "fields": {
+        "id": "int(11) NOT NULL AUTO_INCREMENT",
+        "name" : "varchar(50) NOT NULL",
+        "description" : "varchar(500) NOT NULL",
+        "startTime": "datetime",
+        "endTime": "datetime",
+        "createdAt" : "datetime NOT NULL",
+        "updatedAt" : "datetime NOT NULL"
+    },
+    "constraints": {
+        "PRIMARY KEY (id)"
+    },
+    "post" : [
+    ]
+}
+
+schema.teamUserRel = {
+    "name": "teamUserRel",
+    "fields": {
+        // TODO foreign key to team id
+        "teamId" : "int(11) NOT NULL",
+        // TODO foreign key to user id
+        "userId" : "int(11) NOT NULL",
+        "userType" : "varchar(50) NOT NULL",
+        "createdAt" : "datetime NOT NULL",
+        "updatedAt" : "datetime NOT NULL"
+    },
+    "constraints": {
+        "PRIMARY KEY (id)",
+        "FOREIGN KEY (teamId) REFERENCES teams(id)",
+        "FOREIGN KEY (userId) REFERENCES users(id)",
+        "CONSTRAINT teamUserId UNIQUE (teamId,userId)"
+    },
+    "post" : [
+    ]
+}
+
+schema.scuntUserRel = {
+    "name": "scuntUserRel",
+    "fields": {
+        "scuntId" : "int(11) NOT NULL",
+        "userId" : "int(11) NOT NULL",
+        "createdAt" : "datetime NOT NULL",
+        "updatedAt" : "datetime NOT NULL"
+    },
+    "constraints": {
+        "PRIMARY KEY (id)",
+        "FOREIGN KEY (scuntId) REFERENCES scunt(id)",
+        "FOREIGN KEY (userId) REFERENCES users(id)",
+        "CONSTRAINT scuntUserId UNIQUE (scuntId,userId)"
+    },
+    "post" : [
+    ]
+}
+
+
 
 module.exports = schema;
