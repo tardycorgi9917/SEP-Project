21d377946b78d88e816403e1a86dec3a0e747321
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2017-02-08 15:20:58 -0500

    added travis config file

---
+++ database/config.travis.js
@@ -0,0 +1,11 @@
+var config = {
+    development: {
+        database: {
+            host:'localhost',
+            db:'sep_test',
+            user:'root',
+            password: '',
+        },
+    },
+};
+module.exports = config;
