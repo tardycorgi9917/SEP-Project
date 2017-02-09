95eadccb7d0f04b30bfac47d4ec8726a26bc5c11
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2017-02-09 10:42:42 -0500

    updated test file

--- test/test.js
+++ test/test.js
@@ -2,14 +2,25 @@ var assert = require('assert');
 var db = require('../database/db');
 var seed = require("../database/seeders");
 
-// describe('Database', function() {
-//   describe('schemaUp', function() {
-//     it('It should upload database schema is up', function() {
+describe('Database', function() {
+  describe('schemaUp', function() {
+    it('It should upload database schema is up', function() {
+      db.connect(db, function(err) {
+        if (err) {
+          console.log('Unable to connect to MySQL.')
+          process.exit(1)
+        } else {
+          console.log("DB CONNECTED");
+        }
+      })
 
-//       assert.equal(-1, [1,2,3].indexOf(4));
-//     });
-//   });
-// });
+      db.get().query("", [], function(){
+        
+      });
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
 
 describe('Array', function() {
   describe('#indexOf()', function() {
