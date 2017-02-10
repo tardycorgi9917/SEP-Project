1cb30658109970ecfdda7c5dd9653c333a323975
marc <marc.wang@mail.mcgill.ca>
2017-02-10 12:17:00 -0500

    added update

--- models/scavengerHunts.js
+++ models/scavengerHunts.js
@@ -3,7 +3,8 @@ var db = require("../database/db.js");
 var scunt = {}
 
 scunt.create = function(name, description, startTime, endTime) {
-    var values = [name, description, startTime,endTime, new Date().toISOString().slice(0, 19).replace('T', ' '),new Date().toISOString().slice(0, 19).replace('T', ' ')];
+    var values = [name, description, startTime.toISOString(0,19).replace('T', ' '),endTime.toISOString(0,19).replace('T', ' '), 
+                                    new Date().toISOString().slice(0, 19).replace('T', ' '),new Date().toISOString().slice(0, 19).replace('T', ' ')];
 
     db.get().query('INSERT INTO scunt (name, description, startTime,endTime, createdAt, updatedAt) VALUES(?, ?, ?, ?, ?, ?)', values, function(err, result) {
         if (err) {
@@ -13,8 +14,14 @@ scunt.create = function(name, description, startTime, endTime) {
 }
 
 scunt.update = function(id , name, description, startTime, endTime){
+    var values = [name, description, startTime.toISOString(0,19).replace('T', ' '),endTime.toISOString(0,19).replace('T', ' '), 
+                                                                        new Date().toISOString().slice(0, 19).replace('T', ' '), id];
     
-
+    db.get().query('UPDATE scunt SET name = ?, description = ? , startTime = ?, endTime = ?, updatedAt = ? WHERE id = ?', values, function(err, result) {
+        if (err) {
+            console.log(err)
+        }
+    })
 
 }
 
