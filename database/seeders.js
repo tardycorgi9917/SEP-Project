756cf4e15719c4b642798ee01928238cdf91a858
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2017-02-10 10:29:22 -0500

    DB up and down final version!

--- database/seeders.js
+++ database/seeders.js
@@ -3,62 +3,62 @@ var db = require('./db');
 async = require('async');
 var seed = {};
 
-seed.up = function(done) {
+seed.up = function (done) {
 	var tables = Object.keys(schema);
 	var qstr = "";
-	async.each(tables, function(table) {
+	async.each(tables, function (table) {
 		if (schema.hasOwnProperty(table)) {
-			qstr += "CREATE TABLE IF NOT EXISTS `" + schema[table].name  + "` (";
+			qstr += "CREATE TABLE IF NOT EXISTS `" + schema[table].name + "` (";
 
-			for(var field in schema[table].fields){
+			for (var field in schema[table].fields) {
 				if (schema[table].fields.hasOwnProperty(field)) {
-					 qstr += "`" + field + "` "; // column name
-					 qstr += schema[table].fields[field]; // column options
-					 qstr += ", ";
+					qstr += "`" + field + "` "; // column name
+					qstr += schema[table].fields[field]; // column options
+					qstr += ", ";
 				}
 			}
-			
-			for(var constraint in schema[table].constraints){
+
+			for (var constraint in schema[table].constraints) {
 				qstr += schema[table].constraints[constraint]; // constraint
 				qstr += ", ";
 			}
-			
+
 			qstr = qstr.slice(0, -2); // remove last comma
 			qstr += ");";
 		}
-	}, done);
+	});
 	console.log("****** query to create tables: " + qstr);
 
-	db.get().query(qstr, [], function(err, result) { //query for table structure
+	db.get().query(qstr, [], function (err, result) { //query for table structure
 		var i = 0;
-		if(err){
+		if (err) {
 			console.log("****** it fucked up " + err);
 			console.log("****** it fucked up with " + qstr);
 		} else {
 			console.log("---- Table created Succesfully ----");
 		}
+
+		done();
 	});
 }
 
-seed.down = function(done){
+seed.down = function (done) {
 	var tables = Object.keys(schema);
 	tables.reverse();
 	var query = "";
-	async.each(tables, function(table) {
+	for(var table in schema) {
 		if (schema.hasOwnProperty(table)) {
 			query += "DROP TABLE `" + schema[table].name + "`; ";
 		}
-	}, function(){
-    console.log("****** query to drop tables: " + query);
-
-    db.get().query(query, [], function(err, result) {
-      if(err){
-        console.log("it fucked up " + err);
-       } else {
-        console.log("---- Tables Removed Succesfully ----");
-      }
-      done();
-    });
+	} 
+	console.log("****** query to drop tables: " + query);
+	db.get().query(query, [], function (err, result) {
+		if (err) {
+			console.log("it fucked up " + err);
+		} else {
+			console.log("---- Tables Removed Succesfully ----");
+		}
+		done();
 	});
 }
 
