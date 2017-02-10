5e88fc8e96290b37618619f59dac39f9cd9fb7d3
marc wang <marc.wang@mail.mcgill.com>
2017-02-09 23:41:41 -0500

    sketeton of a code, will work on it tomorrow

---
+++ routes/scavengerHunts.js
@@ -0,0 +1,16 @@
+var express = require('express');
+//var user = require('../models/users.js');
+//var db = require('./../database/db.js')
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('this is the users app');
+});
+
+router.post('/create-ScavengerHunt', function(req, res, next) {
+  //user.create("thomas", "karatzas", "thomas.karatzas@mail.mcgill.ca", function(){});
+  res.send("route working");
+});
+
+module.exports = router;
