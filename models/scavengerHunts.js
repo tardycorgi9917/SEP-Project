440e03d1218d1a488a14803b5f84501833c3b241
marc wang <marc.wang@mail.mcgill.com>
2017-02-11 16:47:01 -0500

    model and route works, need to write DBunit test

--- models/scavengerHunts.js
+++ models/scavengerHunts.js
@@ -3,23 +3,31 @@ var db = require("../database/db.js");
 var scunt = {}
 
 scunt.create = function(name, description, startTime, endTime, done) {
-    var values = [name, description, startTime.slice(0, 19).replace('T', ' '),endTime.slice(0, 19).replace('T', ' '), 
-                                    new Date().toISOString().slice(0, 19).replace('T', ' '),new Date().toISOString().slice(0, 19).replace('T', ' ')];
+
+    var sTime = startTime.toISOString().slice(0, 19).replace('T', ' ');
+    var eTime = endTime.toISOString().slice(0, 19).replace('T', ' ');
+    var CreatedAt = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    var UpdatedAt = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+
+    var values = [name, description,sTime ,eTime, CreatedAt, UpdatedAt];
 
     db.get().query('INSERT INTO scunt (name, description, startTime,endTime, createdAt, updatedAt) VALUES(?, ?, ?, ?, ?, ?)', values, function(err, result) {
         if (err) {
-            console.log(err)
+            done(err, undefined);
+        } else {
+            done(undefined, result.insertId);
         }
     })
 }
 
 scunt.update = function(id , name, description, startTime, endTime, done){
-    var values = [name, description, startTime.slice(0,19).replace('T', ' '),endTime.slice(0,19).replace('T', ' '), 
+    var values = [name, description, startTime.toISOString().slice(0, 19).replace('T', ' '),endTime.toISOString().slice(0, 19).replace('T', ' '), 
                                                                         new Date().toISOString().slice(0, 19).replace('T', ' '), id];
     
     db.get().query('UPDATE scunt SET name = ?, description = ? , startTime = ?, endTime = ?, updatedAt = ? WHERE id = ?', values, function(err, result) {
         if (err) {
-            console.log(err)
+            done(err, undefined);
         }
     })
 
--- routes/scavengerHunts.js
+++ routes/scavengerHunts.js
@@ -9,8 +9,20 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/create-ScavengerHunt', function(req, res, next) {
-  scunt.create("frosh", "fresh meat", new Date(),new Date(), function(){});
-  res.send("route working");
+    var ScuntName = req.query.name;
+    var ScuntDesc = req.query.description;
+    var ScuntStart = new Date(Date.parse(req.query.startTime));
+    var ScuntEnd  = new Date(Date.parse(req.query.endTime));
+
+  scunt.create(ScuntName, ScuntDesc, ScuntStart,ScuntEnd, function(err, id){
+    if(err)
+    {
+      res.status(500).send("An error occurred");
+    }else{
+      res.send(id.toString());
+    }
+   });
 });
+
 
 module.exports = router;
