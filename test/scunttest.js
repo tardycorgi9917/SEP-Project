a1e7db151318bb7b78cb8e41f5652106e6ab1735
marc wang <marc.wang@mail.mcgill.com>
2017-02-12 14:35:18 -0500

    finished the update test

--- test/scunttest.js
+++ test/scunttest.js
@@ -45,6 +45,7 @@ describe('Scunt test', function() {
             assert.strictEqual(result.description,description);
             assert.strictEqual(result.startTime, startTime.toISOString().slice(0,19).replace('T', ' '));
             assert.strictEqual(result.endTime, startTime.toISOString().slice(0,19).replace('T', ' '));
+            assert.strictEqual(result.createdAt, result.updatedAt);
 
           });
           
@@ -53,4 +54,35 @@ describe('Scunt test', function() {
 
       });
   });
+
+
+  describe('Check if scunt update is successfull', function(done){
+    it('Scunt update Succresstul', function(){
+
+      scunt.create('fish frosh', 'jesus loves you', new Date("September 1, 2016 11:13:00"),new Date("September 13, 2016 11:13:00") , function(err, createResult){
+        id = createResult.id;
+
+        assert.strictEqual(err, undefined);
+        var newName = 'NK Frosh';
+        var newDesc = 'North Korea best Korea';
+        var startTime = new Date("September 1, 2017 11:13:00");
+        var endTime = new Date("September 13, 2017 11:13:00");
+
+
+        scunt.update(id,newName, newDesc, startTime, endTime, function(err,modifyResult){
+          assert.strictEqual(err,undefined);
+
+          assert.strictEqual(modifyResult.id,id);
+          assert.strictEqual(modifyResult.description, newDesc);
+          assert.strictEqual(modifyResult.startTime, startTime.toISOString().slice(0,19).replace('T', ' '));
+          assert.strictEqual(modifyResult.endTime, endTime.toISOString().slice(0,19).replace('T', ' '));
+          assert.notStrictEqual(result.createdAt, result.updatedAt);  
+            
+        });
+
+      });
+
+    });
+  });
+
 });
