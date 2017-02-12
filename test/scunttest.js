95af6674d77bcb0aee2160c7abd6160530fc5573
Oleg Zhilin <o.zhilin1@gmail.com>
2017-02-12 18:36:45 -0500

    Fixed tests

--- test/scunttest.js
+++ test/scunttest.js
@@ -5,51 +5,46 @@ var seed = require("../database/seeders");
 
 var scunt = require('../models/scavengerHunts');
 
-
-
-describe('Scunt test', function() {
-
-
-
-  describe('Check if scunt create is succesful', function(done) {
-      it('Scunt creation Successful',function(){
-
-        var name = 'frosh';
-        var description = 'fresh meat';
-        var startTime = new Date("September 1, 2016 11:13:00");
-        var endTime = new Date("September 13, 2016 11:13:00");
-
-        scunt.create(name, description, startTime, endTime , function(err, Result){
-          assert.strictEqual(err, undefined);
-          /*
-          assert.strictEqual(Result.description, description);
-          assert.strictEqual(Result.startTime, startTime.toISOString().slice(0,19).replace('T', ' '));
-          assert.strictEqual(Result.endTime, endTime.toISOString().slice(0,19).replace('T', ' '));               
-          */
-        });       
-
-      });
-
-    it('Scunt update Succresstul', function(){
-
-      scunt.create('fish frosh', 'jesus loves you', new Date("September 1, 2016 11:13:00"),new Date("September 13, 2016 11:13:00") , function(err, id){
-
-        assert.strictEqual(err, undefined);
-        var newName = 'NK Frosh';
-        var newDesc = 'North Korea best Korea';
-        var startTime = new Date("September 1, 2017 11:13:00");
-        var endTime = new Date("September 13, 2017 11:13:00");
-
-
-        scunt.update(id.toString(),newName, newDesc, startTime, endTime, function(err,modifyResult){
-            assert.strictEqual(-1,7);
-            
+describe('Scunt test', function () {
+  before(function (done) {
+    db.connect(db, function (err) {
+      seed.down(function () {
+        seed.up(function () {
+          done();
         });
-
       });
-
-    });      
+    });
   });
 
+  it('Scunt creation Successful', function (done) {
+    var name = 'frosh';
+    var description = 'fresh meat';
+    var startTime = new Date("September 1, 2016 11:13:00");
+    var endTime = new Date("September 13, 2016 11:13:00");
 
+    scunt.create(name, description, startTime, endTime, function (err, resultId) {
+      assert.strictEqual(err, undefined);
+      assert.notStrictEqual(resultId, undefined);
+      // assert.strictEqual(Result.description, description);
+      // assert.strictEqual(Result.startTime, startTime.toISOString().slice(0, 19).replace('T', ' '));
+      // assert.strictEqual(Result.endTime, endTime.toISOString().slice(0, 19).replace('T', ' '));
+      done();
+    });
+
+  });
+
+  it('Scunt update Succresstul', function (done) {
+    scunt.create('fish frosh', 'jesus loves you', new Date("September 1, 2016 11:13:00"), new Date("September 13, 2016 11:13:00"), function (err, id) {
+      assert.strictEqual(err, undefined);
+      var newName = 'NK Frosh';
+      var newDesc = 'North Korea best Korea';
+      var startTime = new Date("September 1, 2017 11:13:00");
+      var endTime = new Date("September 13, 2017 11:13:00");
+
+      scunt.update(id.toString(), newName, newDesc, startTime, endTime, function (err, modifyResult) {
+        assert.strictEqual(-1, 7);
+        done()
+      });
+    });
+  });
 });
--- test/users.js
+++ test/users.js
@@ -4,38 +4,43 @@ var db = require('../database/db');
 var seed = require("../database/seeders");
 var users = require('../models/users');
 
-describe('User Tests', function(){
-    describe('User Creation', function(){
-        it('should create user successfully', function(){
-            var firstName = "FirstName";
-            var lastName = "LastName";
-            var email = "firstname.lastname@gmail.com";
-            var password = "ilikethehabs";
-            var phoneNumber = "(514)911-1234";
-            var profilePicture = "";
-            var date = new Date().toISOString().slice(0, 19).replace('T', ' ');
-            users.create(firstName, lastName, email, password, phoneNumber, profilePicture, date, date,
-                function(err, result){
-                    assert(err, undefined);
-                    assertNotEqual(result.insertId, undefined);
-                }
-            );
-
+describe('User Tests', function () {
+    before(function (done) {
+        db.connect(db, function (err) {
+            seed.down(function () {
+                seed.up(function () {
+                    done();
+                });
+            });
         });
+    });
 
-        it('should not be create user', function(){
-            var firstName = null;
-            var lastName = "LastName";
-            var email = "firstname.lastname@gmail.com";
-            var password = "ilikethehabs";
-            var phoneNumber = "(514) 911-1234";
-            var profilePicture = "";
-            var date = new Date().toISOString().slice(0, 19).replace('T', ' ');
-            users.create(firstName, lastName, email, password, phoneNumber, profilePicture, date, date,
-                function(err, result){
-                    assert.notEqual(err, undefined);
-                }
-            );
+    it('should create user successfully', function () {
+        var firstName = "FirstName";
+        var lastName = "LastName";
+        var email = "firstname.lastname@gmail.com";
+        var password = "ilikethehabs";
+        var phoneNumber = "(514)911-1234";
+        var profilePicture = "";
+        var date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+        users.create(firstName, lastName, email, password, phoneNumber, profilePicture, date, date,
+            function (err, result) {
+                assert(err, undefined);
+                assertNotEqual(result.insertId, undefined);
+            }
+        );
+    });
+
+    it('should not be create user', function () {
+        var firstName = null;
+        var lastName = "LastName";
+        var email = "firstname.lastname@gmail.com";
+        var password = "ilikethehabs";
+        var phoneNumber = "(514) 911-1234";
+        var profilePicture = "";
+        var date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+        users.create(firstName, lastName, email, password, phoneNumber, profilePicture, date, date, function (err, result) {
+            assert.notEqual(err, undefined);
         });
     });
 });
