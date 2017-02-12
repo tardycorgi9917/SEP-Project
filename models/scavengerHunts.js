6a19042ad110cd0aa1118873ca326f7a038afbd8
marc wang <marc.wang@mail.mcgill.com>
2017-02-12 14:55:20 -0500

    fixed the repeating slice date

--- models/scavengerHunts.js
+++ models/scavengerHunts.js
@@ -22,8 +22,12 @@ scunt.create = function(name, description, startTime, endTime, done) {
 }
 
 scunt.update = function(id , name, description, startTime, endTime, done){
-    var values = [name, description, startTime.toISOString().slice(0, 19).replace('T', ' '),endTime.toISOString().slice(0, 19).replace('T', ' '), 
-                                                                        new Date().toISOString().slice(0, 19).replace('T', ' '), id];
+
+    var sTime = startTime.toISOString().slice(0, 19).replace('T', ' ');
+    var eTime = endTime.toISOString().slice(0, 19).replace('T', ' ');
+    var UpdatedAt = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+    var values = [name, description, sTime,eTime, UpdatedAt, id];
     
     db.get().query('UPDATE scunt SET name = ?, description = ? , startTime = ?, endTime = ?, updatedAt = ? WHERE id = ?', values, function(err, result) {
         if (err) {
