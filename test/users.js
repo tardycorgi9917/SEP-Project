32ef2963374576e339ff614fc7f2cb274e0f46bd
Oleg Zhilin <o.zhilin1@gmail.com>
2017-02-12 22:58:09 -0500

    Fixed async issues in new users tests

--- test/users.js
+++ test/users.js
@@ -5,6 +5,14 @@ var seed = require("../database/seeders");
 var users = require('../models/users');
 
 describe('User Tests', function(){
+    before(function (done) {
+        db.connect(db, function (err) {
+            seed.up(function () {
+                done();
+            });
+        });
+    });
+    
     describe('User Creation', function(){
         var firstName = "Eduardo";
         var lastName = "Coronado";
@@ -14,11 +22,12 @@ describe('User Tests', function(){
         var profilePicture = "";
         var date = new Date().toISOString().slice(0, 19).replace('T', ' ');
 
-        it('should create user successfully', function(){
+        it('should create user successfully', function(done){
             users.create(firstName, lastName, email, password, phoneNumber, profilePicture, date, date,
                 function(err, result){
                     assert(err, undefined);
                     assertNotEqual(result.insertId, undefined);
+                    done();
                 }
             );
         });
@@ -31,44 +40,48 @@ describe('User Tests', function(){
         //     );
         // })
 
-        it('should not create user with null name', function(){
+        it('should not create user with null name', function(done){
             nullName = null;
             users.create(nullName, lastName, email, password, phoneNumber, profilePicture, date, date,
                 function(err, result){
                     assert.notEqual(err, undefined);
+                    done();
                 }
             );
         });
       
                 // TODO change
 
-        it('should change phone number', function(){
+        it('should change phone number', function(done){
             var email = "eduardo.coronado@hotmail.com";
             var fields = ["phoneNumber"];
             var values = ["(514)911-4321"];
             users.update(email, fields, values, function(err, result){
                 assert(result.affectedRows, 1);
                 assert(result.changedRows, 1);
+                done();
             });
         });
 
-        it('should change user firstName and lastName', function(){
+        it('should change user firstName and lastName', function(done){
             var email = "eduardo.coronado@hotmail.com";
             var fields = ["firstName", "lastName"];
             var values = ["Michael", "Abdallah"];
             users.update(email, fields, values, function(err, result){
                 assert(result.affectedRows, 1);
                 assert(result.changedRows, 1);
+                done();
             });
         });
 
-        it('should change password', function(){
+        it('should change password', function(done){
             var email = "eduardo.coronado@hotmail.com";
             var fields = ["password"];
             var values = ["vivamexico"];
             users.update(email, fields, values, function(err, result){
                 assert(result.affectedRows, 1);
                 assert(result.changedRows, 1);
+                done();
             });
         });
     });
