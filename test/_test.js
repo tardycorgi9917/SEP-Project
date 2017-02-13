1eedaa2f7c51468455b21c47f5c2e59e895cb38a
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2017-02-13 16:55:09 -0500

    fixed my UNit tests! Thanks Oleg

--- test/_test.js
+++ test/_test.js
@@ -7,43 +7,46 @@ var users = require('../models/users');
 // TODO These tests will have to be revisited to make sure they don't break anything else
 //
 
-// db.connect(db, function (err) {
-//   if (err) console.log('Unable to connect to MySQL.')
+describe('Database', function () {
 
-//   seed.down(function () {
-//     seed.up(done);
-//   });
-//     });
+  before(function (done) {
+    db.connect(db, function (err) {
+      if (err) console.log('Unable to connect to MySQL.')
+      else console.log("DB connected");
+      done();
+    });
+  });
 
-// describe('Database', function() {
+  describe('Check If DB is empty', function () {
+    it('It should ensure DB is empty before seed is called', function (done) {
+      db.get().query("SHOW TABLES", [], function (err, result) {
+        assert.equal(result.length, 0);
+        done();
+      });
+    });
+  });
 
-  // before(function (done) {
-  // });
+  describe('Upload Schema and check if all tables are there', function (done) {
+    it('It should upload database schema', function (done) {
+      // TODO do waterfall test
+      seed.up(function () {
+        db.get().query("SHOW TABLES", [], function (err, result) {
+          assert.equal(result.length, 6);
+          done();
+        });
+      });
+    });
+  });
 
-  // describe('Check If DB is empty', function () {
-  //   it('It should ensure DB is empty before seed is called', function (done) {
-  //     db.get().query("SHOW TABLES", [], function (err, result) {
-  //       assert.equal(result.length, -1);
-  //       done();
-  //     });
-  //   });
-  // });
-
-  // describe('Upload Schema and check if all tables are there', function() {
-  //   it('It should upload database schema', function(done) {
-  //     // TODO do waterfall test
-  //     seed.up(function() {
-  //       db.get().query("SHOW TABLES", [], function(err, result){
-  //         assert.equal(result.length, 6);
-  //         seed.down(function() {
-  //             db.get().query("SHOW TABLES", [], function(err, result){
-  //                 assert.equal(result.length, 0);
-  //                 seed.up(function(){});
-  //                 done();
-  //             });
-  //         });
-  //       });
-  //     });
-  //   });
-// });
-// });
+  describe('Take down schema and make sure DB is empty', function (done) {
+    it('It should emptyt he DB', function (done) {
+      // TODO do waterfall test
+      seed.down(function () {
+        db.get().query("SHOW TABLES", [], function (err, result) {
+          assert.equal(result.length, 0);
+          done();
+        });
+      });
+    });
+  });
+});
