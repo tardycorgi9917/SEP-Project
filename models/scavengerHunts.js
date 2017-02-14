7a0333cd9841fd3605466f34354400ab748bd7ab
marc <marc.wang@mail.mcgill.ca>
2017-02-13 20:26:26 -0500

    added validation if scavenger hunt has same name

--- models/scavengerHunts.js
+++ models/scavengerHunts.js
@@ -2,10 +2,9 @@ var db = require("../database/db.js");
 
 var scunt = {}
 
-scunt.create = function(name, description, startTime, endTime, done) {
+scunt.createScunt = function(name, description, startTime, endTime, done) {
 
-    var sTime = startTime.toISOString().slice(0, 19).replace('T', ' ');
-    var eTime = endTime.toISOString().slice(0, 19).replace('T', ' ');
+
     var date = new Date().toISOString().slice(0, 19).replace('T', ' ');
 
     var values = [name, description,sTime ,eTime, date, date];
@@ -19,6 +18,44 @@ scunt.create = function(name, description, startTime, endTime, done) {
         }
     })
 }
+
+scunt.create = function(name,description, startTime, endTime, done){
+
+    var sTime = startTime.toISOString().slice(0, 19).replace('T', ' ');
+    var eTime = endTime.toISOString().slice(0, 19).replace('T', ' ');
+
+    async.waterfall([
+        //checks if scunt already exist
+        function(callback){
+            var query = 'SELECT COUNT(*) AS duplicateScunt FROM scunt where name =?';
+            var values = [name];
+            db.get().query(query , values, function(err,result){
+                if(err){
+                    callback(err);
+                }else if(result[0].duplicateScunt > 0 )
+                {
+                    callback('Scunt with same name already exist ')
+                }else{
+                    callback(null);
+                }
+
+            });
+
+        },
+        function(callback){
+            scunt.createScunt(name,description, sTime, eTime , done);
+        }
+
+    ],function(err, scuntId){
+
+        done(err,scuntId);
+
+    });
+
+
+
+}
+
 
 scunt.update = function(id , name, description, startTime, endTime, done){
 
