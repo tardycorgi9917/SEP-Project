f4528ddd022fd3118168d92351410f4088b74590
Oleg Zhilin <o.zhilin1@gmail.com>
2017-02-19 17:59:54 -0500

    Fixes here and there

--- models/tasks.js
+++ models/tasks.js
@@ -56,38 +56,42 @@ tasks.create = function(taskName, description, points, scuntId, done) {
 tasks.edit = function(taskID,editDict, done) {
 	console.log('edit');
 	async.waterfall([
-		function(callback) {
-			// Check if there is a task with the same name
-			// might be unnecessary
-			// TODO
-			console.log("test");
-			var query = 'SELECT COUNT(*) AS duplicateTask FROM tasks WHERE id = ?';
-			var values = [taskID];
+		function (callback) {
 
-			db.get().query(query, values, function(err, result) {
+			if (!editDict["name"]) {
+				return callback(null); // cannot be duplicate modif if name not changed
+			}
+
+			var query = 'SELECT COUNT(*) AS duplicateTasks'
+				+ ' FROM tasks t1'
+				+ ' JOIN tasks t2 ON t1.scuntId = t2.scuntId AND t1.id <> t2.id AND t1.id = ? AND t2.name = ?'
+
+			var values = [taskID, editDict["name"]];
+
+			db.get().query(query, values, function (err, result) {
 				if (err) {
 					callback(err);
-				} else if (result[0].duplicateTask == 0) {
-					callback('A task with this name does not exists');
+				} else if (result[0].duplicateTasks > 0) {
+					callback('A duplicate task exists');
 				} else {
 					callback(null);
 				}
 			});
 		},
-		function(callback) {
+		function (callback) {
 			// Create task entry
 			var query = 'UPDATE tasks SET ';
 			for (var field in editDict) {
 				if (editDict.hasOwnProperty(field)) {
-					query += field ; // column name
-					query += "= '"+editDict[field]+"'"; // column options
+					query += field; // column name
+					query += "= '" + editDict[field] + "'"; // column options
 					query += ", ";
 				}
 			}
 
 			var now = new Date().toISOString().slice(0, 19).replace('T', ' ');
-			query += " updatedAt ='"+now
-					+ "' WHERE id = '" + taskID +"';"
+			query += " updatedAt ='" + now
+				+ "' WHERE id = '" + taskID + "';"
 
 			db.get().query(query, function (err, result) {
 				if (err) {
@@ -97,7 +101,7 @@ tasks.edit = function(taskID,editDict, done) {
 				}
 			});
 		}
-	], function(err, taskId) {
+	], function (err, taskId) {
 		done(err, taskId);
 	});
 }
