a823508ab8f68098a56fd1e053b81697b85bd3f5
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2017-03-03 12:43:40 -0500

    started user pop and setup

---
+++ database/populate.js
@@ -0,0 +1,40 @@
+var users = require('../models/users')
+var db = require("../database/db")
+
+var user_pop = [
+    {
+        username : 'stephcurry30',
+        firstName : "Stephen",
+        lastName : "Curry",
+        email : "stephcurry30@gsw.com",
+        password : "splashbro1"
+    },
+    {
+        username : 'klayThomps11',
+        firstName : "Klay",
+        lastName : "Thompson",
+        email : "klaythomps11@gsw.com",
+        password : "splashbro2"
+    },
+    {
+        username : 'draygreen23',
+        firstName : "Draymond",
+        lastName : "Green",
+        email : "draygreen23@gsw.com",
+        password : "spartankick"
+    },
+    {
+        username : 'javaleMcGee1',
+        firstName : "Javale",
+        lastName : "McGee",
+        email : "JavaleMcGee1@gsw.com",
+        password : "shaqtin"
+    },
+    {
+        username : 'KevDurant35',
+        firstName : "Kevin",
+        lastName : "Durant",
+        email : "kevdurant35@gsw.com",
+        password : "durantula"
+    }
+]
