6f8f7c74363c04a5fdc885bf3cbc44149fe4d325
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2017-03-08 15:11:59 -0500

    added task seeding

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ else if (args.length && args[0] == "DB=down") {
 }
 else if(args.length && args[0] == "seed" && args[1] == "-c"){
   console.log("****************************Cleaning and Seeding********************************");
-  seed.down(function () {
+  seed.down(function() {
    seed.up(function() {
      seed.populate(function(){
        console.log("****************************FINISHED POPULATING DROPPING TABLES********************************");
