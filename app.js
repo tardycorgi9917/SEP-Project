8f9ed6b2a538d1704d065dc02026b7d88052c3f3
Marco Manglaviti <marco.manglaviti@gmail.com>
2017-03-29 13:42:28 -0400

    Increase JSON limit

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
