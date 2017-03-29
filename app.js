fbe9bd2e8aca07d84af2077cb7c294cea404948b
Marco Manglaviti <marco.manglaviti@gmail.com>
2017-03-29 13:54:01 -0400

    changed limit

--- app.js
+++ app.js
@@ -79,7 +79,8 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.raw({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb'}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
